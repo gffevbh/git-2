2b9017a3a96b58f21534a24386667a77f417ac98
Dell <kuviktoriia-kustova.1611@mail.ru>
2023-10-06 23:46:49 +0300

    Commit 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.io.PrintStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        sayHello();
+    }
+
+    public static void sayHello() {
+        System.out.print("Hello world!");
     }
 }
